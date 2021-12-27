62c6a43c97763a66823cafb7ee09762a1969f800
Nattamon Polvichai <joppy.pgh@gmail.com>
2021-12-27 19:53:16 +0700

    useEffect tutorial

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  // Used to update stuff outside of the component
+  useEffect(() => {
+    console.log("call useEffect");
+    if (value >= 1) document.title = `New Messages(${value})`;
+  }, [value]);
+
+  // dependency list tells to run useEffect when the specified  values are updated
+  // empty dependency list = run only on initial render
+  // no dependency list = runs initial render and every re-render
+
+  console.log("render");
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  // Returns cleanup function which runs after rerender before useEffect
+  // In this case, we remove the old event listener before creating a new one
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size} px</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
