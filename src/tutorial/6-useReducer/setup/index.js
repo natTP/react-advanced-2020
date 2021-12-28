72f8c0d540e180aa13e2dea9271e75c9b091f880
Nattamon Polvichai <joppy.pgh@gmail.com>
2021-12-28 19:34:45 +0700

    useReducer tutorial

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,73 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
+
+// more complex, structured state
+// Redux
+
 // reducer function
+// a function that will manipulate the state when dispatch is called
+// the state will only be set by the reducer
+// usually put in a different file because it's bulky
+import { reducer } from "./reducer";
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newPerson = { id: new Date().getTime().toString(), name };
+      // dispatch an action to reducer
+      dispatch({ type: "ADD_ITEM", payload: newPerson });
+      setName("");
+    } else {
+      dispatch({ type: "NO_VALUE" });
+    }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
+  };
+
+  return (
+    <>
+      {state.isModalOpen && (
+        <Modal modalContent={state.modalContent} closeModal={closeModal} />
+      )}
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id} className="item">
+            <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
---
+++ src/tutorial/6-useReducer/setup/reducer.js
@@ -0,0 +1,32 @@
+// reducer function
+// a function that will manipulate the state when dispatch is called
+// the state will only be set by the reducer
+
+export const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "item added",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "please enter a value",
+    };
+  }
+  if (action.type === "CLOSE_MODAL") {
+    return { ...state, isModalOpen: false };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    const newPeople = state.people.filter(
+      (person) => person.id != action.payload
+    );
+    return { ...state, people: newPeople };
+  }
+  throw new Error("Action is not handled in reducer");
+};
