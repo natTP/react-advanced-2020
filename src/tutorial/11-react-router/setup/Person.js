8ae8abc3028886f64b768735f54a3d72bb97be27
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-01-01 18:57:47 +0700

    react router tutorial

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
+
 const Person = () => {
+  const [displayName, setDisplayName] = useState("default name");
+  const { name } = useParams(); // Router parameters
+
+  console.log(useParams());
+
+  useEffect(() => {
+    const newPerson = data.find((person) => person.name === name);
+    setDisplayName(newPerson.name);
+  }, []);
+
   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
+      <Link to="/people">Back</Link>
     </div>
   );
 };
